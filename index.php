5255cf76c12688b3010c3a31b144b2d291af06e0
Andres Felipe Fuentes <affuentes@gmail.com>
2018-10-16 13:59:42 -0500

    first_commit_chat_online

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+	include_once('php/config.php');
+	include_once('php/device.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Chat Online</title>
+	<meta charset="UTF-8">
+	<?php 
+		if($tablet_browser > 0){
+		    echo '<link rel="stylesheet" type="text/css" href="'.URL_BASE.'css/stylesMobile.css">';
+		}else if($mobile_browser > 0){
+			echo '<link rel="stylesheet" type="text/css" href="'.URL_BASE.'css/stylesMobile.css">';
+		}else{
+		    echo '<link rel="stylesheet" type="text/css" href="'.URL_BASE.'css/styles.css">';
+		}
+	?>
+	<script type="text/javascript">
+		var urlPort = '<?=SOCKET_FRONTEND;?>';
+	</script>
+	<script src="<?=URL_BASE;?>js/jquery.js" type="text/javascript"></script>
+	<script src="<?=URL_BASE;?>js/fancywebsocket.js"></script>
+	<script src="<?=URL_BASE;?>js/myjava.js" type="text/javascript"></script>
+</head>
+<body>
+	<div id="wrapper">
+		<div id="header">
+			Bienvenido <span id="userName" style="color:#424242;"></span>
+		</div>
+		
+		<div id="content">
+			<ul id="containerMessages"></ul>
+		</div>
+		
+		<div id="footer">
+			<form id="formChat" type="name">
+				<table>
+					<tr>
+						<td width="90%"><input type="text" placeholder="¿Cual es tu nombre?" id="name" autofocus autocomplete="off"></td>
+						<td><input type="submit" value="Ingresar" id="submit"></td>
+					</tr>
+				</table>
+			</form>
+		</div>
+	</div>
+</body>
+</html>
---
+++ php/config.php
@@ -0,0 +1,6 @@
+<?php 
+	define('URL_BASE', 'http://192.168.0.4/chat_online/');
+	define('SOCKET_FRONTEND', '192.168.0.4:12346');
+	define('SOCKET_BACKEND_IP', '192.168.0.4');
+	define('SOCKET_BACKEND_PORT', '12346');
+?>
