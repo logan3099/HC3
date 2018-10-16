b01840fe20d5552323dcaaf4baf7defeceb12516
Andres Felipe Fuentes V <affuentes@gmail.com>
2018-10-16 14:13:31 -0500

    Update send.php

--- php/send.php
+++ php/send.php
@@ -1,8 +1,12 @@
 <?php
+
+	$var;
 	session_start();
 	$name = $_POST['name'];
 	$message = $_POST['message'];
 	$dateTime = '<br><span>'.date('d M Y H:i:s').'<span>';
 	$array[] = array('name' => $name, 'message' => $message, 'sesion' => $_SESSION['sesionName'], 'dateTime' => $dateTime);
 	echo json_encode($array);
+
+	
 ?>
